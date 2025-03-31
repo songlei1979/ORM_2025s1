2ad0ae77abf89de30ee6d5ca0791b0cf19ac59cc
Lei <gabriel_sl19798@hotmail.com>
2025-03-31 18:17:32 +1300

    created generic view for category

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from lesson.views import index, categorylist, post_detail, post_delete
+from lesson.views import index, categorylist, post_detail, post_delete, CategoryList
 
 urlpatterns = [
     path('', index, name='index'),
     path('categorylist/', categorylist, name='category_list'),
     path('post/<int:post_id>', post_detail, name='post_detail'),
-    path('post_delete/<int:post_id>', post_delete, name='post_delete')
+    path('post_delete/<int:post_id>', post_delete, name='post_delete'),
+    path('categorylist_g/', CategoryList.as_view(), name='category_list_g'),
 ]
--- lesson/views.py
+++ lesson/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import ListView
 
 from lesson.models import Post, Category
 
@@ -41,3 +42,8 @@ def post_delete(request, post_id):
     return render(request,
                   'index.html',
                   {'posts': posts})
+
+
+class CategoryList(ListView):
+    template_name = 'categorylist_g.html'
+    model = Category
